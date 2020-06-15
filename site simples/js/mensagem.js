a3f19268a21a31be4e9577f095767ef6017f9655
sighy16 <55985964+sighy16@users.noreply.github.com>
2020-06-15 18:33:13 -0300

    Update mensagem.js

--- site simples/js/mensagem.js
+++ site simples/js/mensagem.js
@@ -3,7 +3,7 @@ function Enviar() {
  
     var nome = document.getElementById("nome");
  
-    if (nome.value != "") {
+    if (nome.value.lenght >5) {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
 
