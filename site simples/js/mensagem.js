9fa0e11eccb6285fd5f4a6d2a84adfd219eb6048
sighy16 <55985964+sighy16@users.noreply.github.com>
2020-06-15 16:19:48 -0300

    Add files via upload

---
+++ site simples/js/mensagem.js
@@ -0,0 +1,38 @@
+
+function Enviar() {
+ 
+    var nome = document.getElementById("nome");
+ 
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+	else{
+   		alert('Prencha os campos por favor!!');
+   	}
+
+}
+function validaForm(frm) {
+ 	if(frm.nome.value == "" || frm.nome.value == null || frm.nome.value.lenght < 3);{
+        //É mostrado um alerta, caso o campo esteja vazio.
+        alert("Por favor, indique o seu nome.");
+        //Foi definido um focus no campo.
+        frm.nome.focus();
+        //o form não é enviado.
+        return false;
+   	
+    }
+    //o campo e-mail precisa de conter: "@", "." e não pode estar vazio
+    if(frm.email.value.indexOf("@") == -1 ||
+      frm.email.valueOf.indexOf(".") == -1 ||
+      frm.email.value == "" ||
+      frm.email.value == null) {
+        alert("Por favor, indique um e-mail válido.");
+        frm.email.focus();
+        return false;
+    }
+    
+}
+
+
+
